7e3d9e1088e7106b322a2d60044d0baba32466de
Ibrahim Umar <ibrahim.umar@hi.no>
2020-07-14 13:45:26 +0200

    Revert "Add Windows version (diff. line endings) of haddock model"
    This reverts commit cbae798b1478d87411cd9724a3287f15469526da.

--- R/loadExample.R
+++ R/loadExample.R
@@ -1,22 +1,5 @@
-# Detect OS
-get_os <- function() {
-	if (.Platform$OS.type == "windows") {
-		"win"
-	} else if (Sys.info()["sysname"] == "Darwin") {
-		"mac"
-	} else if (.Platform$OS.type == "unix") {
-		"unix"
-	} else {
-		stop("Unknown OS")
-	}
-}
-
 loadExample <- function(){
-	if( get_os() == "win" ) {
-		path <- system.file("extdata/haddock-win", package = "gadgetr")
-	} else {
-		path <- system.file("extdata/haddock", package = "gadgetr")
-	}
+ 	path <- system.file("extdata/haddock", package = "gadgetr") 
 	print(path)
 	setwd(path)
 }
